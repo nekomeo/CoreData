30d6489a5dabd8e161cb33188db3c009931dcb18
nekomeo <linh.tu545@gmail.com>
2017-08-13 21:51:24 -0700

    Initial Commit

---
+++ CoreData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreData
+//
+//  Created by Elle Ti on 2017-08-13.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
